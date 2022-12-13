9766d60b468534370c3e3120fdeb3420565c2adb
Daniele Di Sarli <danieleds0@gmail.com>
2022-12-13 12:18:25 +0100

    favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   baseUrl: "/freeswitch/",
   onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
+  favicon: "img/favicon.webp",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
