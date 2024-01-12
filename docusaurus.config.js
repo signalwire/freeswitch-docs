439ec53e4c1a08378c48ba559e253fffc50919d3
August <112662403+hey-august@users.noreply.github.com>
2024-01-12 16:18:12 -0500

    Enable 'Last Updated' date (#94)
    * Initiating
    
    * Abc
    
    * Added a 'Last Updated' date to all pages.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,6 +32,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/signalwire/freeswitch-docs/tree/main/",
+          showLastUpdateTime: true,
         },
         blog: false,
         theme: {
