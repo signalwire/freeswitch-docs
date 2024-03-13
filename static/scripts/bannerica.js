779aabb40ebbc3852bff8d48a92f988ad0d08a0a
Devon <86693904+Devon-White@users.noreply.github.com>
2024-03-13 13:28:35 -0400

    Added Banner script and stylesheet (#107)
    * Added Banner script and stylesheet
    
    * Added Banner script and stylesheet

---
+++ static/scripts/bannerica.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var link = document.createElement("a");
+
+    link.href = "https://signalwire.com/enterprise-connect-2024?x-craft-preview=Pf1n9sLgPn&x-craft-preview=GycqMWdsUN&x-craft-preview=7S4KoPCcLT&x-craft-preview=ymLvN3CqO1&x-craft-preview=BhzWPl7Ntk&x-craft-preview=rHg5bUKLTk&x-craft-preview=rHg5bUKLTk&x-craft-preview=8rBwlgtYiq&utm_source=google&utm_medium=ads&utm_campaign=us_search_brand&utm_content=testing&gad_source=1&gclid=Cj0KCQiAuqKqBhDxARIsAFZELmLpkRL3cC2CMMtFig2QqUyk9m9Ty_on-7WaRZPbLmZNlV41gQmd204aAkoKEALw_wcB"; // Replace with your desired link URL
+    link.target = "_blank";
+    link.id="bannerica"
+
+    document.body.insertBefore(link, document.body.firstChild);
+});
